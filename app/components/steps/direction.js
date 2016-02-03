ce129fa074210068cb9b2876a2cb368346bc4582
Bert Spaan <bert.spaan@gmail.com>
2016-02-03 11:54:43 -0500

    now with field-of-view!

--- app/components/steps/direction.js
+++ app/components/steps/direction.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import L from 'leaflet';
 
+import {fromFeature} from 'field-of-view';
+import turfDistance from 'turf-distance';
+import turfBearing from 'turf-bearing';
+
 import Map from '../map';
 
 const Step = React.createClass({
@@ -35,39 +39,158 @@ const Step = React.createClass({
     );
   },
 
-  mapCreated: function(map) {
-    var mapCoordinates = this.props.stepData.map.geometry.coordinates;
-    var pointList = [
+  getFieldOfView: function(origin, target) {
+    var originFeature = {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: origin
+      }
+    }
+
+    var targetFeature = {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: target
+      }
+    };
+
+    var distance = turfDistance(originFeature, targetFeature) * 1000;
+    var bearing = turfBearing(originFeature, targetFeature);
+
+    return fromFeature({
+      type: 'Feature',
+      properties: {
+        distance: distance,
+        angle: 30,
+        bearing: bearing
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: origin
+      }
+    });
+  },
+
+  getPointList: function(map, point) {
+    var origin = [map.getCenter().lng, map.getCenter().lat];
+    var target = point;
+
+    var vof = this.getFieldOfView(origin, target);
+
+    return [
       [
-        mapCoordinates[1],
-        mapCoordinates[0]
+        vof.geometry.geometries[1].coordinates[0][1],
+        vof.geometry.geometries[1].coordinates[0][0]
       ],
       [
-        mapCoordinates[1],
-        mapCoordinates[0]
+        vof.geometry.geometries[0].coordinates[1],
+        vof.geometry.geometries[0].coordinates[0]
+      ],
+      [
+        vof.geometry.geometries[1].coordinates[1][1],
+        vof.geometry.geometries[1].coordinates[1][0]
       ]
     ];
+  },
 
-    var polyline = L.polyline(pointList , {color: 'red'}).addTo(map);
+  mapCreated: function(map) {
+    var pointList = this.getPointList(map, this.props.stepData.map.geometry.coordinates);
+
+    // Eerste stap:
+    //   Probeer zo precies mogelijk foto in in 't centrum van de cirkel te plaatsen.
+    //      - Kaarten of documenten? Knop: kan niet
+    //      - Andere knop: ik wil nieuwe!
+
+    // Tweede stap:
+    //   Doe de hoek, angle van field-of-view is nog vast
+    //     - knop terug naar vorige stap
+
+    // Derde stap:
+    //   Doe de angle van field-of-view, met sliders
+    //     - knop terug naar vorige stap
+
+    // Twee lijnen: tussen center en twee punten links en rechts van
+    //   coordinaten uit map-stap
+    // En polygoon tussen dezelfde punten, en polygoon
+    //   heeft gradient
+
+    // field-of-view uitbreiden, ook twee punten en angle!
+    //   dus van eerste punt naar tweede punt, met angle
+
+    var polyline = L.polyline(pointList, {color: 'red'}).addTo(map);
+
+    // var polygonPointList = [
+    //   [
+    //     40.760260692426165,
+    //     -73.90914916992188
+    //   ],
+    //   [
+    //     mapCoordinates[1],
+    //     mapCoordinates[0]
+    //   ],
+    //   [
+    //     40.78989968531352,
+    //     -73.96339416503906
+    //   ]
+    // ];
+    //
+    // var polygon = L.polygon(polygonPointList, {
+    //   className: 'VISSEN'
+    // }).addTo(map);
 
     this.setState({
       map: map,
-      polyline: polyline
+      polyline: polyline,
+      // polygon: polygon
     });
+
+    var svg = document.querySelector('svg.leaflet-zoom-animated');
+
+
+
+    var defs = document.createElementNS('http://www.w3.org/2000/svg', 'defs');
+    defs.setAttribute('id', 'gradient-defs');
+
+    svg.appendChild(defs);
+
+    var gradient = document.createElementNS('http://www.w3.org/2000/svg', 'linearGradient');
+    gradient.setAttribute('id', 'chips');
+    gradient.setAttribute('x1', '0%');
+    gradient.setAttribute('x2', '0%');
+    gradient.setAttribute('y1', '0%');
+    gradient.setAttribute('y2', '100%');
+
+    defs.appendChild(gradient);
+
+    //stops
+    var stop1 = document.createElementNS('http://www.w3.org/2000/svg', 'stop');
+    stop1.setAttribute('offset', '0%');
+    stop1.setAttribute("stop-color", 'red');
+    gradient.appendChild(stop1);
+
+    var stop2 = document.createElementNS('http://www.w3.org/2000/svg', 'stop');
+    stop2.setAttribute('offset', '100%');
+    stop2.setAttribute('stop-color', 'yellow');
+    gradient.appendChild(stop2);
+
   },
 
   onMove: function() {
     if (this.state.polyline) {
-      var mapCoordinates = this.props.stepData.map.geometry.coordinates;
-      var pointList = [
-        [
-          mapCoordinates[1],
-          mapCoordinates[0]
-        ],
-        this.state.map.getCenter()
-      ];
+
+      var pointList = this.getPointList(this.state.map, this.props.stepData.map.geometry.coordinates);
+
+
+      // var hond = document.getElementsByClassName('VISSEN')[0];
+      // hond.setAttribute('fill', 'url(#chips)')
+
+
 
       this.state.polyline.setLatLngs(pointList);
+      // this.state.polygon.setLatLngs(polygonPointList);
+
     }
   },
 
