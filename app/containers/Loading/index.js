9d19f812833343f030570fa2fdca069898c8f17e
Bert Spaan <bert.spaan@gmail.com>
2016-06-27 11:41:10 -0400

    animated loading page

--- app/containers/Loading/index.js
+++ app/containers/Loading/index.js
@@ -6,22 +6,39 @@ var nypl = require('images/nypl-white.svg');
 
 export class Loading extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      dots: 0,
+      maxDots: 3
+    };
+  }
+
   render() {
+    var text = `Loading${(new Array(this.state.dots + 1).join('.'))}`;
     return (
       <div className={styles.container}>
         <div className={styles.loading}>
           <img src={nypl} />
-          <span>Loading...</span>
+          <div className={styles.text}>{text}</div>
         </div>
       </div>
     );
   }
-}
 
-function mapDispatchToProps(dispatch) {
-  return {
-    changeRoute: (url) => dispatch(push(url)),
-  };
+  componentDidMount() {
+    this.interval = setInterval(()=> {
+      this.setState({
+        dots: (this.state.dots + 1) % (this.state.maxDots + 1)
+      })
+    }, 500);
+  }
+
+  componentWillUnmount() {
+    if (this.interval) {
+      clearInterval(this.interval)
+    }
+  }
 }
 
 export default Loading;
