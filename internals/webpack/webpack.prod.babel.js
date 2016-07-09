195434badab5a92675c2f0049400e40de9a109b8
Bert Spaan <bert.spaan@gmail.com>
2016-07-08 21:01:24 -0400

    use relative url

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -22,6 +22,7 @@ module.exports = require('./webpack.base.babel')({
 
   // Utilize long-term caching by adding content hashes (not compilation hashes) to compiled assets
   output: {
+    publicPath: '/surveyor/',
     filename: '[name].[chunkhash].js',
     chunkFilename: '[name].[chunkhash].chunk.js',
   },
