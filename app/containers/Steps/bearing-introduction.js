c20d1f12a4821818e7868ae2822e9ad5b6af53c2
Bert Spaan <bert.spaan@gmail.com>
2016-07-22 07:49:39 -0400

    removed old code, changed button text, added introduction svg

--- app/containers/Steps/bearing-introduction.js
+++ app/containers/Steps/bearing-introduction.js
@@ -6,20 +6,27 @@ import Buttons from 'components/Buttons';
 
 import styles from './bearing-introduction.css';
 
+// const explanation = require('../../images/bearing-explanation.svg');
+
 export class Step extends React.Component {
 
   render() {
+    // <p>
+    //   <img className={styles.explanation} src={explanation} />
+    // </p>
+
     return (
       <div className={`${styles.container} sidebar-padding`}>
         <h3 className='centered'>Thanks!</h3>
-        <p>If it's clear which way the camera is pointing, try <a href='javascript:void(0);' onClick={this.next.bind(this)}>setting the camera angle</a>. Otherwise, you can just continue to the <a href="javascript:void(0);" onClick={this.props.skip}>next image</a>.</p>
-
+        <p>
+          If it's clear which way the camera is pointing, try setting the camera angle.
+          Otherwise, you can just continue to the next image.
+        </p>
         <Buttons>
           <Button onClick={this.next.bind(this)} type='secondary'>Set Camera Angle</Button>
         </Buttons>
-
         <Buttons>
-          <Button onClick={this.props.skip} type='primary'>Next Image</Button>
+          <Button onClick={this.props.skip} type='primary'>Skip this step</Button>
         </Buttons>
       </div>
     );
@@ -32,36 +39,3 @@ export class Step extends React.Component {
 }
 
 export default Step;
-
-
-// import React from 'react'
-//
-// var explanation = require('../../images/bearing-explanation.svg')
-//
-// const Step = React.createClass({
-//
-//   render: function() {
-//     return (
-//       <div className='geotagger-step all-margin-top opaque'>
-//         <h1>Thank you!</h1>
-//         <div>
-//           Thanks. Would you also like to try finding the angle/bearing/direction of the image?
-//         </div>
-//         <div>
-//           <img className='bearing-explanation' src={explanation} />
-//         </div>
-//         <div className='buttons'>
-//           <button className='button-red' onClick={this.props.thanks}>No!</button>
-//           <button className='button-green' onClick={this.done}>Yes, please!</button>
-//         </div>
-//       </div>
-//     )
-//   },
-//
-//   done: function() {
-//     this.props.done()
-//   }
-//
-// })
-//
-// export default Step
